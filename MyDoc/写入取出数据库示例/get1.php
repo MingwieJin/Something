ac3070391e5fa4b711a1f01bccc66e45f60b40ae
MingweiJin <mingwei0113@163.com>
2018-07-12 16:00:21 +0800

    [NF]:写入取出数据库示例

---
+++ MyDoc/写入取出数据库示例/get1.php
@@ -0,0 +1,34 @@
+<?php  
+//defined('IN_PHPCMS') or exit('No permission resources.');
+$servername = "localhost";  
+$username = "root";  
+$password = "newinfo2";  
+$dbname = "info2";  
+
+// 创建连接  
+$con =mysqli_connect($servername, $username, $password, $dbname);  
+
+// 检测连接  
+
+  
+$sql = "SELECT * FROM info2certified";  
+$result = mysqli_query($con,$sql);  
+if (!$result) {
+    printf("Error: %s\n", mysqli_error($con));
+    exit();
+}
+
+$jarr = array();
+while ($rows=mysqli_fetch_array($result,MYSQL_ASSOC)){
+    $count=count($rows);//不能在循环语句中，由于每次删除 row数组长度都减小  
+    for($i=0;$i<$count;$i++){  
+        unset($rows[$i]);//删除冗余数据  
+    }
+    array_push($jarr,$rows);
+}
+
+echo $str=json_encode($jarr);//将数组进行json编码
+
+
+mysqli_close($con);
+?> 
---
+++ MyDoc/写入取出数据库示例/push1.php
@@ -0,0 +1,61 @@
+<!--<html>
+<meta charset="utf-8" />
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+you meeting location: <?php echo $_POST["location"]; ?><br>
+you company: <?php echo $_POST["company"]; ?><br>
+you position: <?php echo $_POST["position"]; ?><br>
+Your email: <?php echo $_POST["email"]; ?><br>
+you phone: <?php echo $_POST["phone"]; ?>
+
+</body>
+</html>-->
+
+
+<!--//<?PHP
+//require('../wp-config.php' );	
+//if($_POST[re]) {//如果检查到有register这个提交动作
+//  $sql ="insert into users (name,company) values ('$_POST[name]','$_POST[company]')";//写好SQL插入语句
+//  $sth = $db -> prepare($sql);
+//  $sth -> execute();//这两句是PDO数据库的操作方法，不同的语言不同，就是执行刚才那条SQL语句啦
+//?>-->
+
+
+<?php
+	
+session_start();
+$Passcode = $_POST['PassCode'];
+//if ($_SESSION['authcodeRe1Date'] == date("YmdHi")){
+//    echo "too quick";
+//	exit();
+//}
+ $_SESSION['authcodeRe1Date'] = date("YmdHi");
+
+if ($Passcode=="info2_key") {
+$con = mysql_connect("localhost","root","newinfo2");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+ 
+mysql_select_db("info2", $con);
+ 
+$sql="INSERT INTO info2certified (location, name, company, IDcard, certificate, score)
+VALUES
+('$_POST[location]','$_POST[name]','$_POST[company]','$_POST[IDcard]','$_POST[certificate]','$_POST[score]')";
+ 
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+ 
+mysql_close($con);
+} 
+else {
+     echo "wrong passcode";
+	exit();
+}
+
+?>
