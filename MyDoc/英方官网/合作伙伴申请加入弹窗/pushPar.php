802e34235b3f0029f4e75a32150362a997eb3df6
MingweiJin <mingwei0113@163.com>
2018-07-26 17:52:07 +0800

    [IM]:完成的弹窗表格写入数据库发邮件功能

--- MyDoc/英方官网/合作伙伴申请加入弹窗/pushPar.php
+++ MyDoc/英方官网/合作伙伴申请加入弹窗/pushPar.php
@@ -26,14 +26,14 @@ you phone: <?php echo $_POST["phone"]; ?>
 	
 session_start();
 $Passcode = $_POST['PassCode'];
-//if ($_SESSION['authcodeRe1Date'] == date("YmdHi")){
-//    echo "too quick";
-//	exit();
-//}
+if ($_SESSION['authcodeRe1Date'] == date("YmdHi")){
+    echo "too quick";
+	exit();
+}
  $_SESSION['authcodeRe1Date'] = date("YmdHi");
 
 if ($Passcode=="info2_key") {
-$con = mysql_connect("localhost","root","XXXXXXX");
+$con = mysql_connect("localhost","root","XXXXXXXX");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
@@ -41,16 +41,80 @@ if (!$con)
  
 mysql_select_db("info2", $con);
  
-$sql="INSERT INTO partnerReg (companyname, companysource, country, province, city, phone, email, website, zipcode, fax, will)
+$sql="INSERT INTO partnerReg (companyname, companysource, country, province, city, phone, email, website, will)
 VALUES
-('$_POST[companyname]','$_POST[companysource]','$_POST[country]','$_POST[province]','$_POST[city]','$_POST[phone]','$_POST[email]','$_POST[website]','$_POST[zipcode]','$_POST[fax]','$_POST[will]')";
+('$_POST[companyname]','$_POST[companysource]','$_POST[country]','$_POST[province]','$_POST[city]','$_POST[phone]','$_POST[email]','$_POST[website]','$_POST[will]')";
  
 if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
   }
 echo "1 record added";
- 
+
+//发送邮件
+			$to = "jinmw@info2soft.com";
+			$subject = "合作伙伴报名";
+			$message = "
+			<html>
+			<head>
+			<title>合作伙伴报名详情</title>
+			</head>
+			<body>
+			<p>或作伙伴信息:</p>
+			<table>
+			<tr>
+			<td width=70px;>公司名称</td>
+			<td>".$_POST[companyname]."</td>
+			</tr>
+			<tr>
+			<td>母公司名称</td>
+			<td>".$_POST[companysource]."</td>
+			</tr>
+			<tr>
+			<td>国家</td>
+			<td>".$_POST[country]."</td>
+			</tr>
+			<tr>
+			<td>省份</td>
+			<td>".$_POST[province]."</td>
+			</tr>
+			<tr>
+			<td>城市</td>
+			<td>".$_POST[city]."</td>
+			</tr>
+			<tr>
+			<td>电话</td>
+			<td>".$_POST[phone]."</td>
+			</tr>
+                        <tr>
+			<td>邮件</td>
+			<td>".$_POST[email]."</td>
+			</tr>
+                        <tr>
+			<td>网站</td>
+			<td>".$_POST[website]."</td>
+			</tr>
+                        <tr>
+			<td>合作意愿</td>
+			<td>".$_POST[will]."</td>
+			</tr>
+			</table>
+			</body>
+			</html>
+			";
+
+			// 当发送 HTML 电子邮件时，请始终设置 content-type
+			$headers = "MIME-Version: 1.0" . "\r\n";
+			$headers .= "Content-type:text/html;charset=utf-8" . "\r\n";
+
+			// 更多报头
+			$headers .= 'From: <928062736@qq.com>' . "\r\n";
+//			$headers .= 'Cc: chengq@info2soft.com' . "\r\n";
+//			$headers .= 'Cc: jinmw@info2soft.com' . "\r\n";
+
+			mail($to,$subject,$message,$headers);
+//发送邮件结束
+
 mysql_close($con);
 } 
 else {
