c28113208ef9cbc9687c2389629667de85d37114
MingweiJin <mingwei0113@163.com>
2018-09-03 18:08:33 +0800

    [IM]:继续完善购物车

--- Vuejs-learning/vue-shopcar/素材/js/cart.js
+++ Vuejs-learning/vue-shopcar/素材/js/cart.js
@@ -3,9 +3,9 @@
 	data:{
 		totalMoney:0,
 		productList:[],
-//		checkALlFlag:false,
-//		delFlag:false,
-//		curProduct:''
+		checkAllFlag:false,
+		delFlag:false,
+		curProduct:''
 	},
 	filters:{
 		fomatMoney:function(value){
@@ -27,52 +27,53 @@
 				
 			});
 		},
-//		changeMoney:function(product,way){
-//			if(way>0){
-//				product.productQuantity++;
-//			}else{	
-//				if(product.productQuantity>1){
-//					product.productQuantity--;
-//				}
-//			}
-//			this.calcTotalPrice();
-//		},
-//		selectedProduct:function(item){
-//			if(typeof item.checked=='undefined'){
-//				Vue.set(item,'checked',true);
-//			}else{
-//				item.checked = !item.checked;
-//			}
-//			this.calcTotalPrice();
-//		},
-//		checkAll:function(flag){
-//			this.checkAllFlag = flag;
-//			var _this=this;
-//			console.log(this);
-//			
-//			this.productList.forEach(function(item,index){
-//				if(typeof item.checked == 'undefined'){
-//					_this.$set(item,"checked",_this.checkAllFlag);
-//					console.log(this);
-//				}else{
-//					item.checked = _this.checkAllFlag;
-//				}
-//			});
-//			_this.calcTotalPrice();
-//		},
-//		calcTotalPrice:function(){
-//			var _this=this;
-//			this.totalMoney=0;
-//			this.productList.forEach(function(item,index){
-//			if(item.checked){
-//				_this.totalMoney+=item.productPrice*item.productQuantity;
-//			}
-//			});
-//		},
-//		delProduct:function(){
-//			this.productList.splice(this.pindex,1);
-//			this.delFlag=false;
-//		}
+		changeMoney:function(product,way){
+			if(way>0){
+				product.productQuantity++;
+			}else{	
+				if(product.productQuantity>1){
+					product.productQuantity--;
+				}
+			}
+			this.calcTotalPrice();
+		},
+		selectedProduct:function(item){
+			if(typeof item.checked=='undefined'){
+				Vue.set(item,'checked',true);
+			}else{
+				item.checked = !item.checked;
+			}
+			this.calcTotalPrice();
+		},
+		checkAll:function(flag){
+			this.checkAllFlag = flag;
+			var _this=this;
+			console.log(this);
+			
+			this.productList.forEach(function(item,index){
+				if(typeof item.checked == 'undefined'){
+					_this.$set(item,"checked",_this.checkAllFlag);
+					console.log(this);
+				}else{
+					item.checked = _this.checkAllFlag;
+				}
+			});
+			_this.calcTotalPrice();
+		},
+		calcTotalPrice:function(){
+			var _this=this;
+			this.totalMoney=0;
+			this.productList.forEach(function(item,index){
+			if(item.checked){
+				_this.totalMoney+=item.productPrice*item.productQuantity;
+			}
+			});
+		},
+		delProduct:function(){
+			this.productList.splice(this.pindex,1);
+			this.delFlag=false;
+			this.calcTotalPrice();
+		}
 	}
 });
  Vue.filter("money",function(value,type){
