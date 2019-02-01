6ceed5e90829159ec647d3f5f1f766d1c4ab1537
MingweiJin <mingwei0113@163.com>
2019-02-01 16:29:38 +0800

    [NF]:首页头部+轮播图制作

--- Vuejs-learning/Gowhere_Myproject/src/pages/home/home.vue
+++ Vuejs-learning/Gowhere_Myproject/src/pages/home/home.vue
@@ -1,15 +1,18 @@
 <template>
 	<div>
 		<home-header></home-header>
+		<home-swiper></home-swiper>
 	</div>
 </template>
 
 <script>
-	import HomeHeader from './components/header'
+import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
 export default{
 	name:"home",
 	components:{
-		HomeHeader:HomeHeader
+		HomeHeader:HomeHeader,
+		HomeSwiper:HomeSwiper
 	}
 }
 
