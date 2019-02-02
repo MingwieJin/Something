fa46747b63edbe7a00874112ba15540b54ed996d
MingweiJin <mingwei0113@163.com>
2019-02-02 11:38:30 +0800

    [IM]:热门推荐模块开发完成

--- Vuejs-learning/Gowhere_Myproject/src/pages/home/components/recommend.vue
+++ Vuejs-learning/Gowhere_Myproject/src/pages/home/components/recommend.vue
@@ -1,13 +1,81 @@
 <template>
-	<div>111</div>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+				<img class="item-img" :src="item.url">
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default{
 	name:'HomeRecommend',
+	data:function(){
+		return{
+			recommendList:[{
+			id:'0001',
+			url:'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+			title:'故宫',
+			desc:'世界五大宫之首，穿越与您近在咫尺'
+			},{
+			id:'0002',
+			url:'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+			title:'故宫2',
+			desc:'世界五大宫之首，穿越与您近在咫尺'
+			},{
+			id:'0003',
+			url:'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+			title:'故宫3',
+			desc:'世界五大宫之首，穿越与您近在咫尺'
+			},
+			
+			
+			]
+		}
+	}
+	
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
 </style>
