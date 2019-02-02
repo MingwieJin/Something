392471a8033ca89c1ad0a75aac5d5abd92d148ff
MingweiJin <mingwei0113@163.com>
2019-02-02 10:51:43 +0800

    [NF]:推荐部分开始

--- Vuejs-learning/Gowhere_Myproject/src/pages/home/home.vue
+++ Vuejs-learning/Gowhere_Myproject/src/pages/home/home.vue
@@ -3,6 +3,7 @@
 		<home-header></home-header>
 		<home-swiper></home-swiper>
 		<home-icons></home-icons>
+		<home-recommend></home-recommend>
 	</div>
 </template>
 
@@ -10,13 +11,15 @@
 import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
 import HomeIcons from './components/icons'
+import HomeRecommend from './components/recommend'
 
 export default{
 	name:"home",
 	components:{
 		HomeHeader:HomeHeader,
 		HomeSwiper:HomeSwiper,
-		HomeIcons:HomeIcons
+		HomeIcons:HomeIcons,
+		HomeRecommend:HomeRecommend
 	}
 }
 
