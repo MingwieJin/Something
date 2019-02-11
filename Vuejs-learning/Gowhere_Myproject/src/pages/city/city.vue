9a389233a7587297bb0840d59cf2de103dbbbe32
MingweiJin <mingwei0113@163.com>
2019-02-11 18:32:56 +0800

    [NF]:新的切换城市页面

---
+++ Vuejs-learning/Gowhere_Myproject/src/pages/city/city.vue
@@ -0,0 +1,39 @@
+<template>
+    <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/CityHeader'
+import axios from 'axios'
+
+export default{
+  name: 'City',
+  components: {
+    CityHeader: CityHeader
+  },
+  data: function () {
+    return {
+      hotCities: []
+    }
+  },
+  methods: {
+    getcityInfo () {
+      axios.get('/api/city.json')
+        .then(this.getcityInfoSucc)
+    },
+    getcityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getcityInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
