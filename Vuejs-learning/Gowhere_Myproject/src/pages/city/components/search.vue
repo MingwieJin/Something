3389651b01f0167bf2f66690a10bf31fbb863110
MingweiJin <mingwei0113@163.com>
2019-02-13 11:32:02 +0800

    [NF]:城市搜索功能

--- Vuejs-learning/Gowhere_Myproject/src/pages/city/components/search.vue
+++ Vuejs-learning/Gowhere_Myproject/src/pages/city/components/search.vue
@@ -1,28 +1,92 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+	<div>
+	    <div class="search">
+	        <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+	    </div>
+	    <div class="search-content" ref="search" v-show="keyword">
+	    	<ul>
+	    		<li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+	    		<li class="search-item border-bottom" v-show="hasNodata">没有找到匹配数据</li>
+	    	</ul>
+	    </div>
     </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'	
+	
 export default{
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+  	cities: Object
+  },
+  data: function() {
+  	return {
+  		keyword: '',
+  		list: [],
+  		timer: null
+  	}
+  },
+  watch: {
+  	keyword (){
+		if (this.timer){
+			clearTimeout(this.timer)
+		}
+		if (!this.keyword) {
+			this.list = []
+			return
+		}
+		this.timer = setTimeout(()=>{
+			const result = []
+			for (let i in this.cities){
+				this.cities[i].forEach((value) =>{
+					if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1){
+						result.push(value)
+					}
+				})
+			}
+			this.list = result
+		}, 100)  		
+  	}
+  },
+  mounted () {
+  	this.scroll = new BScroll(this.$refs.search)
+  },
+  computed: {
+  	hasNodata(){
+  		return !this.list.length
+  	}
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .search
-   height:.72rem
-   padding:0 .1rem
-   background:$bgColor
-   .search-input
-    width:100%
-    height:.62rem
-    line-height:.62rem
-    text-align:center
-    border-radius:.06rem
-    color:#666
-    padding:0 .1rem
-    box-sizing:border-box
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
+      color: #666
 </style>
